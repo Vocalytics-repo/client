c7f76f4cf0fb97cbc549a0e92451220236c2c09c
TUAZAR <100247125+TUAZAR@users.noreply.github.com>
2025-06-06 10:50:26 +0900

    add: e-learning route

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import STTService from './pages/STTService/STTService';
+import ELearning from './pages/ELearning/ELearning';
 import SidebarMenu from './components/Sidebar/SidebarMenu';
 
 // 임시 페이지 컴포넌트
 const InsightService = () => (
     <div style={{ padding: '50px', maxWidth: '800px', margin: '0 auto', textAlign: 'center' }}>
         <h1>인사이트 서비스</h1>
-        <p>이 페이지는 현재 개발 중입니다.</p>
-    </div>
-);
-
-const ELearningService = () => (
-    <div style={{ padding: '50px', maxWidth: '800px', margin: '0 auto', textAlign: 'center' }}>
-        <h1>E러닝 서비스</h1>
         <p>이 페이지는 현재 개발 중입니다.</p>
     </div>
 );
@@ -30,7 +24,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/stt" element={<STTService />} />
                 <Route path="/insights" element={<InsightService />} />
-                <Route path="/elearning" element={<ELearningService />} />
+                <Route path="/elearning" element={<ELearning />} />
             </Routes>
         </Router>
     );
